88dbb5b8da967bdc360af9b772a5c89c5e6c57d7
vanderleik <vanderleik@yahoo.com.br>
2023-08-30 17:49:56 -0300

    Add Categories in DB

--- src/App.js
+++ src/App.js
@@ -182,11 +182,66 @@ function App() {
           </div>
           <div class="card-body">
             <div class="text-center">
-              <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="assets/images/product_dummy.svg" alt="image dummy"></img>
+              {/* <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="assets/images/product_dummy.svg" alt="image dummy"></img> */}
             </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
             <a target="_blank" rel="nofollow" href="/">View product detail</a>
           </div>
+        </div>
+      </div>
+
+      {/* Categories in DB */}
+      <div class="col-lg-6 mb-4">
+        <div class="card shadow mb-4">
+          <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">Categories in Data Base</h6>
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6 mb-4">
+                  <div class="card bg-info text-white shadow">
+                    <div class="card-body">
+                      Category 01
+                    </div>
+                  </div>
+                  <div class="col-lg-6 mb-4">
+											<div class="card bg-info text-white shadow">
+												<div class="card-body">
+													Category 02
+												</div>
+											</div>
+										</div>
+                    <div class="col-lg-6 mb-4">
+											<div class="card bg-info text-white shadow">
+												<div class="card-body">
+													Category 03
+												</div>
+											</div>
+										</div>
+                    <div class="col-lg-6 mb-4">
+											<div class="card bg-info text-white shadow">
+												<div class="card-body">
+													Category 04
+												</div>
+											</div>
+										</div>
+                    <div class="col-lg-6 mb-4">
+											<div class="card bg-info text-white shadow">
+												<div class="card-body">
+													Category 05
+												</div>
+											</div>
+										</div>
+                    <div class="col-lg-6 mb-4">
+                      <div class="card bg-info text-white shadow">
+                        <div class="card-body">
+													Category 06
+												</div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
         </div>
       </div>
         
