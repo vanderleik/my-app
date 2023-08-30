af84ada5eb141129f580526f6b70886a064ca194
vanderleik <vanderleik@yahoo.com.br>
2023-08-30 17:31:53 -0300

    Add $$$ of all products in DB

--- src/App.js
+++ src/App.js
@@ -135,6 +135,24 @@ function App() {
         </div>
       </div>
 
+      {/* $$$ of all products in DB */}
+      <div class="col-md-4 mb-4">
+							<div class="card border-left-success shadow h-100 py-2">
+								<div class="card-body">
+									<div class="row no-gutters align-items-center">
+										<div class="col mr-2">
+											<div class="text-xs font-weight-bold text-success text-uppercase mb-1"> Amount in products</div>
+											<div class="h5 mb-0 font-weight-bold text-gray-800">$546.456</div>
+										</div>
+										<div class="col-auto">
+											<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+      
+
       </div>
 
       </div>
