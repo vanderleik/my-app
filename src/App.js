c8973ba6dac41dc24eacdac12028eb74e37bb961
vanderleik <vanderleik@yahoo.com.br>
2023-08-30 17:29:51 -0300

    Add Amount of Products in DB

--- src/App.js
+++ src/App.js
@@ -115,6 +115,28 @@ function App() {
         <h1 class="h3 mb-0 text-gray-800">App Dashboard</h1>
       </div>
 
+      {/* Content Row */}
+      <div class="row">
+
+      {/* Amount of Products in DB */}
+      <div class="col-md-4 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"> Products in Data Base</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">135</div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      </div>
+
       </div>
 
       </div>
